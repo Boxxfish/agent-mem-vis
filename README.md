d605493053609b17d7009d32a97586de41770d0a
Ben Giacalone <bensgiacalone@gmail.com>
2023-12-26 17:27:34 -0500

    Baseline commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Agent Memory Visualizer
+
+A web UI for visualizing past trajectories retrieved by an agent.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Agent Memory Visualizer</title>
+</svelte:head>
+
+<div>
+	Test
+</div>
+
+<style>
+	
+</style>
